dc5d4111331a6707ecc40e71c84490c8250c9231
Mehwish Shakeel <160312242+Mehwishshakeel@users.noreply.github.com>
2024-03-06 15:40:49 +0500

    Add files via upload

---
+++ Q10.js
@@ -0,0 +1,9 @@
+"use strict";
+// Add two numbers
+console.log(6 + 2);
+// Subtraction of two numbers
+console.log(16 - 8);
+// Multiplication of two numbers
+console.log(2 * 4);
+// Divide two numbers
+console.log(16 / 2);
